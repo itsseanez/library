4401eca91162351b806e8ce6282255b3a66c2b19
Sean Ezenwa <seanezenwa@gmail.com>
2023-01-31 22:20:24 -0500

    Complete grid layout

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let myLibrary = [];
 
-function Movie() {
+function Book() {
   // the constructor...
 }
 
-function addMovieToLibrary() {
+function addBookToLibrary() {
   // do stuff here
+}
+
+//dispays books in array on page
+function displayBooks() {
+    for(let i=0; i< myLibrary.length; i++) {
+
+    }
 }
