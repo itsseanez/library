99e24ed641d6f10dbe990199d09f9c5f7af13d7c
Sean Ezenwa <seanezenwa@gmail.com>
2023-02-04 11:51:23 -0500

    Add new book button and style js code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+const book1 = new Book("Dazai osamu","no longer human", 125, true);
+
+let myLibrary = [book1];
 
 function Book(author, title, pageNumbers, isRead) {
   // the constructor...
@@ -6,7 +8,6 @@ function Book(author, title, pageNumbers, isRead) {
   this.title= title;
   this.pageNumbers= pageNumbers;
   this.isRead= isRead;
-  return title;
 }
 
 function addBookToLibrary() {
@@ -18,5 +19,23 @@ function displayBooks() {
     for(let i=0; i< myLibrary.length; i++) {
       const mainContent= document.querySelector('#main-content');
       const book= document.createElement('div');
+      const title= document.createElement('p')
+      const author= document.createElement('p');
+      const pages= document.createElement('p');
+      
+      title.textContent= myLibrary[i].title;
+      author.textContent= myLibrary[i].author;
+      pages.textContent= myLibrary[i].pageNumber;
+      
+      book.classList.add('book');
+      title.classList.add('text');
+      author.classList.add('text');
+      pages.classList.add('text');
+      book.appendChild(title);
+      book.appendChild(author);
+      book.appendChild(pages);
+      mainContent.appendChild(book);
     }
 }
+
+displayBooks();
