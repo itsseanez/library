132db0a070ecee2e83da684e174d2636a19308eb
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-12-03 17:12:53 -0500

    Create funcitionality to add a new book

--- script.js
+++ script.js
@@ -35,9 +35,21 @@ function displayLibrary() {
         pages.textContent = `Pages: ${book.pages}`;
         bookDiv.appendChild(pages);
 
-        const isRead = document.createElement('p');
-        isRead.textContent = `Read: ${book.isRead ? 'Yes' : 'No'}`;
+        const isRead = document.createElement('button');
+        isRead.textContent = book.isRead ? 'Read' : 'Not Read';
+        if (book.isRead) {
+            isRead.classList.add('clicked');
+        }
+        isRead.addEventListener('click', () => {
+            book.isRead = !book.isRead;
+            isRead.textContent = book.isRead ? 'Read' : 'Not Read';
+            isRead.classList.toggle('clicked');
+        });
         bookDiv.appendChild(isRead);
+
+        /*const deleteButton = document.createElement('button');
+        deleteButton.textContent = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>;
+        bookDiv.appendChild(deleteButton);*/
 
         libraryContainer.appendChild(bookDiv);
     });
@@ -47,3 +59,29 @@ function displayLibrary() {
 addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
 addBookToLibrary('1984', 'George Orwell', 328, false);
 displayLibrary();
+
+const dialog = document.querySelector("dialog");
+const openDialogButton = document.getElementById('open-dialog-btn');
+const closeDialogButton = document.getElementById("close-dialog-btn");
+const submitBookButton = document.getElementById("submit-book-btn");
+
+submitBookButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isRead = document.getElementById("isRead").checked;
+
+    addBookToLibrary(title, author, pages, isRead);
+    displayLibrary();
+    dialog.close();
+    document.getElementById("book-form").reset();
+});
+
+closeDialogButton.addEventListener('click', () => {
+    dialog.close();
+});
+
+openDialogButton.addEventListener('click', () => {
+    dialog.showModal();
+});
